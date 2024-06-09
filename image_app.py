9b67d5b19d442eba5534a56c9b45f1050e661f5c
Georgi Varbanov <gvarbanov@usf.edu>
2024-06-09 19:41:17 -0400

    changed some code within image_py to retrieve uploaded image but still unable to do it

--- image_app.py
+++ image_app.py
@@ -1,19 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import os
 
 app = Flask(__name__)
 
+app.config['uploads'] = '/Users/gv/code/general_projects/changing_images/uploads'
+
 @app.route('/', methods=['GET', 'POST'])
 def idex():
     if request.method == 'POST':
-        file = request.files['filename']
-        if file:
-            #save file to directory
-            file_path = os.path.join('uploads', file.filename)
-            file.save(file_path)
+        image = request.files['file']
 
+        if image.filename == '':
+            print("Invalid submission")
+            return redirect(request.url)
+
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        image.save(os.path.join(basedir, app.config['uploads'], image.filename))
             ###PERFORM OTHER OPERATIONS HERE###
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
