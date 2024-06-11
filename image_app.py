bd9717c83cf487378a9e79aba692654bdd7a2f72
Georgi Varbanov <gvarbanov@usf.edu>
2024-06-10 20:50:16 -0400

    continued working on image_app, still trying to retrieve image

--- image_app.py
+++ image_app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, request, redirect
 import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-app.config['uploads'] = '/Users/gv/code/general_projects/changing_images/uploads'
+UPLOAD_FOLDER = '/Users/gv/code/general_projects/changing_images/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit ('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 def idex():
