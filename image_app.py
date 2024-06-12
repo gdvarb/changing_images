1550bfb9e84959f92424b5bd2a053bb1298fc9ae
Georgi Varbanov <gvarbanov@usf.edu>
2024-06-12 14:17:12 -0400

    changed html and python code so that an image can now be uploaded and is saved in the uploads folder at the specified directory

--- image_app.py
+++ image_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash, url_for
+from flask import Flask, render_template, request, redirect, flash, url_for, render_template_string
 import os
 from werkzeug.utils import secure_filename
 
@@ -12,33 +12,33 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit ('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
+    print(f"request path {request.path}")
     if request.method == 'POST':
-        
-        if 'file' not in request.files:
-            flash('No file part')
+
+        if 'filename' not in request.files:
             return redirect(request.url)
         
-        image = request.files['file']
 
-        if image.filename == '':
-            print("Invalid submission")
-            return redirect(request.url)
+        file = request.files['filename']
 
-        if image and allowed_file(image.filename):
-            filename = secure_filename(image.filename)
-            image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        if file.filename == '':
+            return 'No selected file'
 
-        return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filepath)
+            return redirect(url_for('index'))
+
+    return render_template('index.html')
 
 if __name__ == '__main__':
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
     app.run(host = 'localhost', port=8080, debug = True)
